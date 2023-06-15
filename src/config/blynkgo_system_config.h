0482c1d87ebd9ec7c0aeb71b73940d0c9592761b
BlynkGO <65327357+BlynkGO@users.noreply.github.com>
2023-06-15 13:25:27 +0700

    Update blynkgo_system_config.h

--- src/config/blynkgo_system_config.h
+++ src/config/blynkgo_system_config.h
@@ -531,6 +531,7 @@
   #define TFT_RST                 -1
   #define TFT_BUSY                -1
   #define TFT_BL                  27 // 2.8 : 21 
+  #define TFT_BL_INVERT           false
   #define TFT_INVERT              false
   #define TFT_SPI_3WIRE           false
   #define TFT_DUMMY_READ_PIXEL    8
