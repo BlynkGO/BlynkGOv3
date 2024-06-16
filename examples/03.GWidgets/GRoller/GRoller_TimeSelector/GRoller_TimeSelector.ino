520445877106d3ea1fd5cb9c16f9037081bb5579
TridentTD <prote2000@gmail.com>
2024-06-16 09:04:22 +0700

    Update GRoller_TimeSelector.ino

--- examples/03.GWidgets/GRoller/GRoller_TimeSelector/GRoller_TimeSelector.ino
+++ examples/03.GWidgets/GRoller/GRoller_TimeSelector/GRoller_TimeSelector.ino
@@ -6,23 +6,23 @@ GRoller roller_second;
 
 GLabel lbClock;
 
-uint8_t hour   = 0;
-uint8_t minute = 0;
-uint8_t second = 0;
+uint8_t hh = 0;
+uint8_t mm = 0;
+uint8_t ss = 0;
 
 
 void roller_onValueChange(GWidget *widget){
-  hour    = roller_hour   .selected().toInt();
-  minute  = roller_minute .selected().toInt();
-  second  = roller_second .selected().toInt();
+  hh  = roller_hour   .selected().toInt();
+  mm  = roller_minute .selected().toInt();
+  ss  = roller_second .selected().toInt();
 
-  lbClock = StringX::printf("เวลา %02d:%02d:%02d", hour, minute, second);
+  lbClock = StringX::printf("เวลา %02d:%02d:%02d", hh, mm, ss);
 }
 
 void setup() {
   Serial.begin(115200);
   BlynkGO.begin();
-  lbClock  = StringX::printf("เวลา %02d:%02d:%02d", hour, minute, second);
+  lbClock  = StringX::printf("เวลา %02d:%02d:%02d", hh, mm, ss);
   lbClock.font(prasanmit_35);
   lbClock.color(TFT_WHITE);
   
@@ -43,7 +43,7 @@ void setup() {
 
   uint16_t w =  8 + roller_minute.width() + 8 + roller_second.width();
   lbClock      .align( ALIGN_IN_TOP_MID,0,10);        // จัดตำแหน่ง GLabel
-  roller_hour  .align( lbDate      , ALIGN_BOTTOM  ,-w/2 , 10  );
+  roller_hour  .align( lbClock      , ALIGN_BOTTOM  ,-w/2 , 10  );
   roller_minute.align( roller_hour  , ALIGN_RIGHT   , 8  , 0   ); 
   roller_second.align( roller_minute, ALIGN_RIGHT   , 8  , 0   );
 }
