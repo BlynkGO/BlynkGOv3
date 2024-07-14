d93caf61e96c56c4eb35a6b94f60e128e495aca6
TridentTD <prote2000@gmail.com>
2024-07-14 10:13:12 +0700

    v3.0.22

--- src/BlynkGOv3.h
+++ src/BlynkGOv3.h
@@ -132,7 +132,9 @@
  *        แก้ไขปัญหา BlynkGO_AsyncMQTT32 เมื่อเชื่อมต่อได้แล้ว แต่พอเข้าไป GWiFiManager เพื่อปิด SSID 
  *        มีอาการตาย reset ตัวเอง  และ เมื่อปิดแล้วแก้ได้แล้วแต่พอจะเปิดใหม่มีอาการหน่วงๆ  ได้แก้ทั้ง 2 issues นี้แล้ว
  *        --> BlynkGO_AsyncMQTT32 v1.0.3
- * 
+ *   [V3.0.22] @14/07/24
+ *      - platformio v3.0.21 ไม่รับ ติด catch ? อัพเป็น 3.0.22 แทน
+ *
  *********************************************************************
  */
 
@@ -144,7 +146,7 @@
 /** Minor version number (x.X.x) */
 #define BLYNKGO_VERSION_MINOR   0
 /** Patch version number (x.x.X) */
-#define BLYNKGO_VERSION_PATCH   21
+#define BLYNKGO_VERSION_PATCH   22
 
 #define BLYNKGO_VERSION_TEXT    (String(BLYNKGO_VERSION_MAJOR)+"."+String(BLYNKGO_VERSION_MINOR)+"."+String(BLYNKGO_VERSION_PATCH))
 
